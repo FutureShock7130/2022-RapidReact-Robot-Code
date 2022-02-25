01dca162f0df47ae0e3c1de55a430bf3d3b8d84e
Alb33rt <albert931023@gmail.com>
2022-02-25 20:14:38 +0800

    AutoModePlanner.java modified

--- src/main/java/frc/robot/auto/AutoModePlanner.java
+++ src/main/java/frc/robot/auto/AutoModePlanner.java
@@ -1,1 +1,29 @@
 package frc.robot.auto;
+
+public class AutoModePlanner {
+    private enum DriveMode {
+        MECANUM,
+        DIFFERENTIAL,
+        ARCADE,
+        TANK
+    }
+
+    private enum AimMode {
+        LIMELIGHT, 
+        ABSOLUTE,
+        RELATIVE
+    }
+
+    private enum Alliance {
+        RED_ALLIANCE,
+        BLUE_ALLIANCE
+    }
+
+    private enum DrivePath {
+        ONE_CARGO,
+        TWO_CARGO,
+        THREE_CARGO,
+        FOUR_CARGO,
+        FIVE_CARGO
+    }
+}
