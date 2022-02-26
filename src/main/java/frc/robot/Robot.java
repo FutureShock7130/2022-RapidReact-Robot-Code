7e5cc4991bd322f54c4c0954df4d1e5926811452
Alb33rt <albert931023@gmail.com>
2022-02-26 19:36:47 +0800

    Added Feedforward Test Functionality

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -91,5 +91,6 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
