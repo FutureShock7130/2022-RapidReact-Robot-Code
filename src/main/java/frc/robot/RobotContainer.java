56bd5e13239cec94aec4f91de0878176497590f9
AaronWangTW <68408616+AaronWangTW@users.noreply.github.com>
2022-03-02 10:36:53 +0800

    minor change

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -253,23 +253,6 @@ public class RobotContainer {
         Trajectory trajectory;
         trajectory = PathPlanner.loadPath("Straight Test Path", DriveConstants.kMaxVelocityMetersPerSecond, DriveConstants.kMaxAccelerationMetersPerSecondSquared);
 
-        // try {
-        //     trajectory = PathPlanner.loadPath("Straight Test Path", DriveConstants.kMaxVelocityMetersPerSecond, DriveConstants.kMaxAccelerationMetersPerSecondSquared);
-        // } catch (IOException ex) {
-        //     DriverStation.reportError("Unable to open trajectory: " + "Straight Test Path", ex.getStackTrace());
-        //     trajectory =
-        //     TrajectoryGenerator.generateTrajectory(
-        //         // Start at the origin facing the +X direction
-        //         new Pose2d(0, 0, new Rotation2d(0)),
-        //         // Pass through these two interior waypoints, making an 's' curve path
-        //         List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        //         // End 3 meters straight ahead of where we started, facing forward
-        //         new Pose2d(3, 0, new Rotation2d(0)),
-        //         // Pass config
-        //         config);
-        // }
-        // An example trajectory to follow.  All units in meters.
-
         RamseteCommand ramseteCommand = new RamseteCommand(
             trajectory,
             m_robotDrive::getDifferentialPose,
