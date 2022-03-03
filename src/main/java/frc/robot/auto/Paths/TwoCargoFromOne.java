303479567618da9c54e625d34e9df5385f091225
frank931023 <frank931023@gmail.com>
2022-03-03 23:37:30 +0800

    two cargo from one auto finished

--- src/main/java/frc/robot/auto/Paths/TwoCargoFromOne.java
+++ src/main/java/frc/robot/auto/Paths/TwoCargoFromOne.java
@@ -1,26 +1,53 @@
 package frc.robot.auto.Paths;
 
+import javax.xml.namespace.QName;
+
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
 import frc.robot.commands.Intake.TimedIntake;
+import frc.robot.commands.Transporter.TimedTransport;
+import frc.robot.commands.Turret.TimedTurret;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Transporter;
+import frc.robot.subsystems.Turret;
 
 public class TwoCargoFromOne {
-    RobotContainer m_robot;
+    Intake m_robotIntake;
     TrajectoryGenerator trajectoryGenerator;
+    Transporter m_robotTransport;
+    Drive m_robotDrive;
+    Turret m_robotTurret;
 
-    public TwoCargoFromOne(RobotContainer robot) {
-        m_robot = robot;
-        trajectoryGenerator = new TrajectoryGenerator(m_robot.m_robotDrive);
+    public TwoCargoFromOne(RobotContainer robot, Transporter m_robotTransport, Intake m_robotIntake, Drive m_robotDrive,
+            Turret m_robotTurret) {
+        this.m_robotTransport = m_robotTransport;
+        this.m_robotTransport = m_robotTransport;
+        this.m_robotDrive = m_robotDrive;
+        this.m_robotIntake = m_robotIntake;
+        this.m_robotTurret = m_robotTurret;
+        trajectoryGenerator = new TrajectoryGenerator(this.m_robotDrive);
     }
 
     public SequentialCommandGroup getCommand() {
         return new SequentialCommandGroup(
+
                 new ParallelCommandGroup(
                         trajectoryGenerator.generateTranslationalPrimary("1 to 2 from 1",
                                 new PIDController(0.03, 0.003, 0.003), new PIDController(0.03, 0.003, 0.003)),
-                        new TimedIntake(2.0, m_robot.m_robotIntake)));
-    }
+                        new TimedIntake(2.0, this.m_robotIntake)),
 
+                new ParallelCommandGroup(
+                        trajectoryGenerator.generateTranslationalPrimary("2 to 2s from 1",
+                                new PIDController(0.03, 0.003, 0.003), new PIDController(0.03, 0.003, 0.003)),
+                        new SequentialCommandGroup(
+                                new TimedIntake(0.5, this.m_robotIntake),
+                                new WaitCommand(0.5),
+                                new TimedTransport(1, this.m_robotTransport)),
+                        new TimedTurret(this.m_robotTurret, 1, 1750)));
+    }
 }
