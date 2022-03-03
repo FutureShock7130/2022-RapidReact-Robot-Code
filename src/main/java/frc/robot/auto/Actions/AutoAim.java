838ae6d0e39baa4b2e378332c8c53a9a3e84cc79
AaronWangTW <68408616+AaronWangTW@users.noreply.github.com>
2022-03-03 17:09:27 +0800

    AutoAim Command Group

---
+++ src/main/java/frc/robot/auto/Actions/AutoAim.java
@@ -0,0 +1,34 @@
+package frc.robot.auto.Actions;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Drive.AbsoluteAim;
+import frc.robot.commands.Turret.LimelightAim;
+import frc.robot.commands.Turret.TurretSeek;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Spinner;
+import frc.robot.subsystems.Turret;
+import frc.robot.vision.Limelight;
+
+public class AutoAim {
+
+    private Drive m_robotDrive;
+    private Spinner m_Spinner;
+    private Limelight m_vision;
+
+    public AutoAim(Drive robotDrive, Spinner robotSpinner, Limelight vision){
+        m_robotDrive = robotDrive;
+        m_Spinner = robotSpinner;
+        m_vision = vision;
+    }
+    
+    public Command getCommand(){
+        SequentialCommandGroup command = new SequentialCommandGroup(
+            //new AbsoluteAim(m_robotDrive),
+            new TurretSeek(m_Spinner, m_vision),
+            new LimelightAim(m_vision, m_Spinner)
+        );
+        return command;
+    }
+
+}
