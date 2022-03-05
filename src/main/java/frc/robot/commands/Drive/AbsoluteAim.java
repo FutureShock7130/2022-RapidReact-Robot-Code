5f1d27f19a82caa4f04241af2f30a876717227ff
Alb33rt <albert931023@gmail.com>
2022-03-05 15:53:42 +0800

    Changed v value down to a worse value

--- src/main/java/frc/robot/commands/Drive/AbsoluteAim.java
+++ src/main/java/frc/robot/commands/Drive/AbsoluteAim.java
@@ -28,7 +28,7 @@ public class AbsoluteAim extends CommandBase {
         double targetX = 0;
         double targetY = -1;
 
-        private static final double kP = 0.52;
+        private static final double kP = 0.4;
         private static final double kI = 0.025;
         private static final double kD = 0.04;
         private static final double timeDiff = 0.02;
